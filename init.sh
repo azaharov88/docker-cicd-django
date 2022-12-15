9a48dd533f7466e322175a414305284780731e18
Alex <zah_al@sh.ru>
2022-12-15 15:42:07 +0700

    add dockerfile

--- init.sh
+++ init.sh
@@ -11,5 +11,6 @@ file=db/db.sqlite3
 #  echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
 #fi
 
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
 python3 manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
+
